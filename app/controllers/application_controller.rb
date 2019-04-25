f5c16d2206ce5cc3d2a43ee974d841db93b5a20c
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-25 17:16:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < Sinatra::Base
      if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
-      #User.create(username: params[:username], password: params[:password])
       redirect '/login'
     end
 end
@@ -36,7 +35,10 @@ end
   end
 
   post "/login" do
-    ##your code here
+   if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      redirect '/'
   end
 
   get "/failure" do
